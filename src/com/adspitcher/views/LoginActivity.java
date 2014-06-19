8ca83e752fa18704854fc49811e2cafb97ed9310
rachna <rachnakhokhar@gmail.com>
2014-06-19 18:25:48 +0530

    Code written for switching login to signup. Added files specific to Action Bar. New resources added.

--- src/com/adspitcher/views/LoginActivity.java
+++ src/com/adspitcher/views/LoginActivity.java
@@ -1,12 +1,16 @@
 package com.adspitcher.views;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.adspitcher.R;
 
@@ -26,6 +30,19 @@ public class LoginActivity extends Activity{
 		EditText password = (EditText) findViewById(R.id.edittext_password);
 		password.setTypeface(Typeface.SERIF);
 		password.setTransformationMethod(new PasswordTransformationMethod());
+		
+		//Action on click of Create An Account Button
+		TextView textview_createaccount = (TextView) findViewById(R.id.textview_createaccount);
+		textview_createaccount.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View view) {
+				Intent screenChangeIntent = null;
+				screenChangeIntent = new Intent(LoginActivity.this,
+						SignupActivity.class);
+				LoginActivity.this.startActivity(screenChangeIntent);
+			}
+		});
 	}
 
 }
---
+++ src/com/adspitcher/views/SignupActivity.java
@@ -0,0 +1,17 @@
+package com.adspitcher.views;
+
+import com.adspitcher.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SignupActivity extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_signup);
+	}
+
+}
