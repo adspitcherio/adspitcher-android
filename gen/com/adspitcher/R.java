0abd58063362a9ecd0d5f266ea90c3364c2ead6e
rachna <rachnakhokhar@gmail.com>
2014-06-20 09:48:18 +0530

    Code written to change UI of Login and Signup.

--- gen/com/adspitcher/R.java
+++ gen/com/adspitcher/R.java
@@ -1167,6 +1167,7 @@ containing a value of this type.
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
         public static final int btn_login=0x7f05003f;
+        public static final int btn_submit=0x7f050044;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
@@ -1174,14 +1175,10 @@ containing a value of this type.
         public static final int disableHome=0x7f050008;
         public static final int dropdown=0x7f05000f;
         public static final int edit_query=0x7f050030;
-        public static final int edittext_dob=0x7f050049;
-        public static final int edittext_email=0x7f050047;
-        public static final int edittext_firstnamelastname=0x7f050043;
+        public static final int edittext_email=0x7f050042;
+        public static final int edittext_name=0x7f050041;
         public static final int edittext_password=0x7f05003e;
-        public static final int edittext_phonenumber=0x7f050048;
-        public static final int edittext_retype_password=0x7f050046;
-        public static final int edittext_signup_password=0x7f050045;
-        public static final int edittext_signup_username=0x7f050044;
+        public static final int edittext_signup_password=0x7f050043;
         public static final int edittext_username=0x7f05003d;
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
@@ -1192,7 +1189,7 @@ containing a value of this type.
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
         public static final int imageview_loginlogo=0x7f05003c;
-        public static final int imageview_splash=0x7f05004a;
+        public static final int imageview_splash=0x7f050045;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
@@ -1220,9 +1217,7 @@ containing a value of this type.
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
         public static final int textview_createaccount=0x7f050040;
-        public static final int textview_registerlater=0x7f050041;
-        public static final int textview_taptoaddpic=0x7f050042;
-        public static final int textview_version=0x7f05004b;
+        public static final int textview_version=0x7f050046;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
@@ -1328,16 +1323,18 @@ containing a value of this type.
         public static final int app_name=0x7f0a000d;
         public static final int cd_img_addpic=0x7f0a000f;
         public static final int cd_img_splash=0x7f0a000e;
-        public static final int text_create_account=0x7f0a0013;
-        public static final int text_dob=0x7f0a001b;
-        public static final int text_email=0x7f0a0019;
-        public static final int text_fullname=0x7f0a0017;
-        public static final int text_password=0x7f0a0012;
-        public static final int text_phonenumber=0x7f0a001a;
-        public static final int text_register_later=0x7f0a0014;
-        public static final int text_repeatpassword=0x7f0a0018;
-        public static final int text_signin=0x7f0a0015;
-        public static final int text_taptoaddpic=0x7f0a0016;
+        public static final int text_create_account=0x7f0a0014;
+        public static final int text_dob=0x7f0a001d;
+        public static final int text_email=0x7f0a001b;
+        public static final int text_fullname=0x7f0a0019;
+        public static final int text_name=0x7f0a0012;
+        public static final int text_password=0x7f0a0013;
+        public static final int text_phonenumber=0x7f0a001c;
+        public static final int text_register_later=0x7f0a0015;
+        public static final int text_repeatpassword=0x7f0a001a;
+        public static final int text_signin=0x7f0a0016;
+        public static final int text_submit=0x7f0a0017;
+        public static final int text_taptoaddpic=0x7f0a0018;
         public static final int text_username=0x7f0a0011;
         public static final int text_version=0x7f0a0010;
     }
@@ -1446,6 +1443,7 @@ containing a value of this type.
         public static final int TextAppearance_Widget_AppCompat_ExpandedMenu_Item=0x7f0b0028;
         public static final int Theme_Adp=0x7f0b0085;
         /**  this style is only referenced in a Light.DarkActionBar based theme 
+ this style is only referenced in a Light.DarkActionBar based theme 
  this style is only referenced in a Light.DarkActionBar based theme 
          */
         public static final int Theme_Adp_Widget=0x7f0b008e;
