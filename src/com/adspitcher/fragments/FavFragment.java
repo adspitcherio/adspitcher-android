07cd748ab03b9e1d3884ddfdbcee8c0579da0262
rachna <rachnakhokhar@gmail.com>
2014-06-20 18:08:47 +0530

    Code written for Nearby and Fav fragments.

--- src/com/adspitcher/fragments/FavFragment.java
+++ src/com/adspitcher/fragments/FavFragment.java
@@ -1,12 +1,15 @@
 package com.adspitcher.fragments;
 
 import com.adspitcher.R;
+import com.adspitcher.adapters.OffersFragmentAdapter;
+import com.adspitcher.dataobjects.OffersItem;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 public class FavFragment extends Fragment{
 
@@ -15,6 +18,17 @@ public class FavFragment extends Fragment{
 			Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		View view = inflater.inflate(R.layout.fragment_fav, container, false);
+		
+		ListView listView_fav_offers_items = (ListView)view.findViewById(R.id.listview_favoffers);
+		
+		OffersItem[] offersDataItems = new OffersItem[10];
+		for (int i = 0; i < 10; i++) {
+			offersDataItems[i] = new OffersItem("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", "Pantaloons", "Gurgaon | Mumbai", 10, 10, 10, 10);
+		}
+		
+		OffersFragmentAdapter adapter = new OffersFragmentAdapter(view.getContext(), R.layout.fragment_fav, offersDataItems);
+		listView_fav_offers_items.setAdapter(adapter);
+		
 		return view;
 	}
 }
--- src/com/adspitcher/fragments/NearbyFragment.java
+++ src/com/adspitcher/fragments/NearbyFragment.java
@@ -5,8 +5,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.adspitcher.R;
+import com.adspitcher.adapters.OffersFragmentAdapter;
+import com.adspitcher.dataobjects.OffersItem;
 
 public class NearbyFragment  extends Fragment{
 
@@ -15,6 +18,17 @@ public class NearbyFragment  extends Fragment{
 			Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		View view = inflater.inflate(R.layout.fragment_nearby, container, false);
+		
+		ListView listView_nearby_offers_items = (ListView)view.findViewById(R.id.listview_nearbyoffers);
+		
+		OffersItem[] offersDataItems = new OffersItem[10];
+		for (int i = 0; i < 10; i++) {
+			offersDataItems[i] = new OffersItem("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", "Pantaloons", "Gurgaon | Mumbai", 10, 10, 10, 10);
+		}
+		
+		OffersFragmentAdapter adapter = new OffersFragmentAdapter(view.getContext(), R.layout.fragment_nearby, offersDataItems);
+		listView_nearby_offers_items.setAdapter(adapter);
+		
 		return view;
 	}
 }
