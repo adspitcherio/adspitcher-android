99c5a8e4fdc949abab04f1ecb2e0c83dbd02366d
rachna <rachnakhokhar@gmail.com>
2014-06-20 09:52:19 +0530

    Code written to navigate to home activity from login and signup.

--- src/com/adspitcher/views/LoginActivity.java
+++ src/com/adspitcher/views/LoginActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -30,6 +31,19 @@ public class LoginActivity extends Activity{
 		EditText password = (EditText) findViewById(R.id.edittext_password);
 		password.setTypeface(Typeface.SERIF);
 		password.setTransformationMethod(new PasswordTransformationMethod());
+		
+		Button btn_signin = (Button) findViewById(R.id.btn_login);
+		btn_signin.setOnClickListener(new  OnClickListener() {
+			
+			@Override
+			public void onClick(View view) {
+				Intent screenChangeIntent = null;
+				screenChangeIntent = new Intent(LoginActivity.this,
+						HomeActivity.class);
+				LoginActivity.this.startActivity(screenChangeIntent);
+				LoginActivity.this.finish();
+			}
+		});
 		
 		//Action on click of Create An Account Button
 		TextView textview_createaccount = (TextView) findViewById(R.id.textview_createaccount);
--- src/com/adspitcher/views/SignupActivity.java
+++ src/com/adspitcher/views/SignupActivity.java
@@ -3,6 +3,7 @@ package com.adspitcher.views;
 import com.adspitcher.R;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SignupActivity extends Activity{
@@ -12,6 +13,12 @@ public class SignupActivity extends Activity{
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_signup);
+		
+		Intent screenChangeIntent = null;
+		screenChangeIntent = new Intent(SignupActivity.this,
+				HomeActivity.class);
+		SignupActivity.this.startActivity(screenChangeIntent);
+		SignupActivity.this.finish();
 	}
 
 }
