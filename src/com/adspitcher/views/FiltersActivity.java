2b6e35905e3704e26cf8b0ee0f5a33cb85cf5bc8
rachna <rachnakhokhar@gmail.com>
2014-06-27 16:33:59 +0530

    Code written for filter screen.

--- src/com/adspitcher/views/FiltersActivity.java
+++ src/com/adspitcher/views/FiltersActivity.java
@@ -1,7 +1,13 @@
 package com.adspitcher.views;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.adspitcher.R;
@@ -15,6 +21,19 @@ public class FiltersActivity extends ActionBarActivity {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.activity_filter);
+		
+		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+		    // get the parent view of home (app icon) imageview
+		    ViewGroup home = (ViewGroup) findViewById(android.R.id.home).getParent();
+		    // get the first child (up imageview)
+		    ( (ImageView) home.getChildAt(0) )
+		        // change the icon according to your needs
+		        .setImageDrawable(getResources().getDrawable(R.drawable.ic_action_content_remove));
+		} else {
+		    // get the up imageview directly with R.id.up
+		    ( (ImageView) findViewById(R.id.up) )
+		        .setImageDrawable(getResources().getDrawable(R.drawable.ic_action_content_remove));
+		} 
 
 		ListView listView_location_filters = (ListView) findViewById(R.id.listView_filters_locations);
 
@@ -48,5 +67,25 @@ public class FiltersActivity extends ActionBarActivity {
 		FiltersAdapter categories_adapter = new FiltersAdapter(this,
 				R.layout.activity_filter, categories_filters);
 		listView_categories_filters.setAdapter(categories_adapter);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu items for use in the action bar
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.activity_filter_menu, menu);
+	    return super.onCreateOptionsMenu(menu);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle presses on the action bar items
+	    switch (item.getItemId()) {
+	        case R.id.action_apply:{
+	            return true;
+	        }
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
 	}
 }
